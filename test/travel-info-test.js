df8441e3842b44421f6283e41609b7521d03beb9
Trond Makonese <trondation@gmail.com>
2020-02-27 10:50:37 -0700

    test for all trips default properties are now passing.

--- test/travel-info-test.js
+++ test/travel-info-test.js
@@ -14,15 +14,46 @@ describe ('Travel Info', () => {
    travelInfo = new TravelInfo(tripsData, destinationsData)
  });
 
-describe ('default properties', () => {
-
-
   it('should be a function', () => {
     expect(TravelInfo).to.be.a('function');
   })
 
   it('should be an instance of a UserName', () => {
     expect(travelInfo).to.be.an.instanceof(TravelInfo);
+  })
+describe.only ('trips default properties', () => {
+
+  it('should have a unique id', () => {
+    console.log(travelInfo.tripsData[0])
+    expect(travelInfo.tripsData[0].id).to.equal(1);
+  })
+
+  it('should have a userId relating to a user', () => {
+    expect(travelInfo.tripsData[0].userID).to.equal(44);
+  })
+
+  it('should have a destination ID', () => {
+    expect(travelInfo.tripsData[0].destinationID).to.equal(49);
+  })
+
+  it('should have a number of travelers', () => {
+    expect(travelInfo.tripsData[0].travelers).to.equal(1);
+  })
+
+  it('should have a travel date', () => {
+    expect(travelInfo.tripsData[0].date).to.equal('2019/09/16');
+  })
+
+  it('should have a trip duration', () => {
+    expect(travelInfo.tripsData[0].duration).to.equal(8);
+  })
+
+  it('should have a status', () => {
+    expect(travelInfo.tripsData[0].status).to.equal('approved');
+  })
+
+  it('should have a list of suggested activities', () => {
+    expect(travelInfo.tripsData[0].suggestedActivities).to.deep.equal([]);
   })
 
 })
