ba82f55e5783c45c7053b2a22c30459248be87e5
Trond Makonese <trondation@gmail.com>
2020-02-26 18:26:31 -0700

    Agent and travelers class are now inheriting from the travel info class.

--- src/travel-agent.js
+++ src/travel-agent.js
@@ -1,5 +1,8 @@
-class TravelAgent {
-  constructor(travelersData) {
+import TravelInfo from '../src/travel-info.js';
+
+class TravelAgent extends TravelInfo {
+  constructor(tripsData, destinationsData, travelersData) {
+    super(tripsData, destinationsData)
     this.travelersData = travelersData;
   }
 }
