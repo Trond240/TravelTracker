bfa0ba1b13e7fc52a88d9d76723f7ed24ac70435
Trond Makonese <trondation@gmail.com>
2020-02-27 13:25:05 -0700

    fix method that gets our user trips destination name.

--- src/travel-info.js
+++ src/travel-info.js
@@ -11,7 +11,7 @@ class TravelInfo {
   getTravelersInformation(userID) {
     return this.tripsData.reduce((tripsList, trips) => {
       if(userID === trips.userID && trips.status === 'approved') {
-        tripsList.push({destination: trips.destinationID, 'Number of Travelers': trips.travelers, date: trips.date})
+        tripsList.push({destination: trips.destinationID, travelers: trips.travelers, date: trips.date})
       }
       return tripsList
     }, [])
@@ -19,7 +19,6 @@ class TravelInfo {
 
   getDistinationName(userID) {
     let findDestinationName = this.getTravelersInformation(userID);
-    console.log(findDestinationName)
     return findDestinationName.reduce((newList, trip) => {
       this.destinationsData.forEach(destination => {
         if(trip.destination === destination.id) {
