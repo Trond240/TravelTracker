05560c4ef78c507573fdaaecabf0d0052259d2c8
Trond Makonese <trondation@gmail.com>
2020-03-01 16:44:01 -0700

    Manager dash board now appears populates.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -27,19 +27,36 @@ export const domUpdates = {
    })
  },
 
- // displayUserTripInfo(travelHistory) {
- //   return travelHistory.forEach(trip => {
- //     console.log(trip)
- //     $('.past-and-present').append(
- //       `<div class='all-trips'>
- //         <p>Destination: ${trip.destination}</p>
- //         <p>Flight Cost Per Person: $${trip.flightPerPerson}.00</p>
- //         <p>Estimated Cost For Lodge: $${trip.costPerPersonADay}.00</p>
- //         <p>Number of Travelers: ${trip.numberOfTravelers}</p>
- //         <p>Date: ${trip.date}</p>
- //       </div>
- //       `)
- //   })
+ displayUserTripInfo(foundInfo) {
+   console.log(foundInfo)
+   return foundInfo.forEach(trip => {
+     console.log(trip)
+     $('.user-pending-trips').append(
+       `<div class='users-trips'>
+         <p>User: ${trip.name}</p>
+         <p>Trip Date: ${trip.tripDate}</p>
+         <p>Trip Status: ${trip.status}</p>
+         <button id='${trip.tripID}'>Approve Trip</button>
+         <button id='${trip.tripID}'>Denny Trip</button>
+       </div>
+       `)
+   })
+ },
+
+ displayManagerTrips(tripsData) {
+   return tripsData.forEach(trip => {
+     $('.manager-trips').append(
+       `<div class='vacation-card'>
+       <h1>Destination:${trip.destination}</h1>
+       <button id='${trip.id}'class='card-image'>
+       <image class='destination-image' src='${trip.image}' alt='click to view recipe for ${trip.alt}'>
+       </button>
+       <p>Estimated Cost Per Day: $${trip.estimatedLodgingCostPerDay}.00</p>
+       <p>Estimated Flight Cost Per Person: $${trip.estimatedFlightCostPerPerson}.00</p>
+       </div>
+       ` )
+   })
+ },
 
  displayAllTrips(tripsData) {
    return tripsData.forEach(trip => {
